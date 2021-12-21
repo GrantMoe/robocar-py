78529d45ad4ed5c4a8c0a02cf8523375d98a87c0
Grant Moe <grant@grantmoe.com>
2021-12-21 11:04:29 -0800

    rewrite test.py to echo HID events

--- test.py
+++ test.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 import asyncio, evdev
 
-tx = evdev.InputDevice('/dev/input/event5')
+# mouse = evdev.InputDevice('/dev/input/event4')
+dev = evdev.InputDevice('/dev/input/event17')
 
 async def print_events(device):
     async for event in device.async_read_loop():
         print(device.path, evdev.categorize(event), sep=': ')
 
-asyncio.ensure_future(print_events(tx))
+# for device in mouse, keybd:
+asyncio.ensure_future(print_events(dev))
 
 loop = asyncio.get_event_loop()
 loop.run_forever()
