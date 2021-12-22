8543927d122a546bd838a561e3e75a27c02102e4
grantmoe <grant@grantmoe.com>
2021-12-22 13:29:02 -0800

    Create config file with default arguments and lists

---
+++ config.py
@@ -0,0 +1,47 @@
+
+# TODO: THIS IS FOR THE LAPTOP, NOT NANO
+default_controller_path = '/dev/input/event17'
+
+# THIS IS FOR NANO
+default_serial_port = "/dev/ttyTHS1"
+
+controller_types = [
+    'xbox',
+    'ble_gamepad'
+]
+default_controller_type = 'ble_gamepad'
+
+
+# Menus
+# format 'm', size, [a, d, e, m], bool(s), value(s)
+AUTO_MENU = {
+    'a': 'toggle_throttle',
+    'b': ['start', 'pause'], # is_driving
+    'main': ['manual', 'auto'],  # auto_throttle
+    'mode': 'autonomous',
+    'status': ['paused', 'driving'], # is_driving
+}
+
+DATA_MENU = {
+    'a': 'erase_records',
+    'b': ['start', 'pause'],  # is_recording
+    'main': 'number of records', # records
+    'mode': 'data',
+    'status': ['paused', 'recording'], # is_recording    
+}
+
+ERASE_MENU = {  # is_erasing
+    'a': 'confirm',
+    'b': 'cancel',
+    'main': 'seconds to erase', # seconds
+    'mode': 'data',
+    'status': ['erasing'],    
+}
+
+MANUAL_MENU = {
+    'a': '',
+    'b': '',
+    'main': '',
+    'mode': 'manual',
+    'status': '',    
+}
