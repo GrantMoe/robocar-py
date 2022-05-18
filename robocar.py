ad3618d07f6a1fb53dde7b9a6fddcae9df4c6199
grantmoe <grant@grantmoe.com>
2022-05-18 15:45:54 -0700

    Tweaked on Nano, working kinda

--- robocar.py
+++ robocar.py
@@ -4,7 +4,7 @@ import time
 import io
 
 
-def main(conf):
+def main():
 
     steering_in = None
     throttle_in = None
@@ -17,24 +17,47 @@ def main(conf):
     os.makedirs(data_dir, exist_ok=True)
 
     # pyserial.readthedocs.io/en/latest/shortintro.html
-    ser = serial.Serial('/dev/ttyACM0', timeout=1)
+    #ser = serial.Serial('/dev/ttyTHS1', timeout=1)
+
+    # start serial with Teensy
+    serial_port = serial.Serial(
+        port='/dev/ttyTHS1',
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+    )
+    time.sleep(1)
+    serial_port.flush()
+
+    print('setup complete')
+
+    serial_port.write(b's')
 
     while True:
-
+        print(f'{input_requested = }, {input_needed = }')
 
         if input_needed and not input_requested:
-            ser.write(b'g')
+            serial_port.write(b'g')
             input_requested = True
 
-        if ser.in_waiting > 0:
-            line = str(ser.readline())
+        if serial_port.in_waiting > 0:
+            bytes_in = serial_port.readline()
+            print(f'{bytes_in = }')            
+            line = str(bytes_in)
             print(f'serial line: {line}')
+            inputs = line[1:].split(',')
+            print(f'{inputs = }')
 
             if line[0] == 'i':
-                inputs = line[1:].split(',')
+                #inputs = line[1:].split(',')
                 steering_in = inputs[0]
                 throttle_in = inputs[1]
                 switch_in = inputs[2]
+                input_needed = True
+                input_requested = False
+
+        time.sleep(1)
 
 
     # # start serial with Teensy
@@ -63,4 +86,6 @@ def main(conf):
     # process drive status
     # get GPS data
     # afs
-     
+
+if __name__ == "__main__":
+    main()
